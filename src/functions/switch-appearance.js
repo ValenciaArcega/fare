6fa5cbc839640b41cd955f2d2d491a32a3d22e1b
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2023-05-30 19:11:57 -0600

    fixes: some style issues with dark mode and also some variables had been changed on name

--- src/functions/switch-appearance.js
+++ src/functions/switch-appearance.js
@@ -11,15 +11,16 @@ export function makeDark() {
   root.style.setProperty('--black18', '#fff');;
   root.style.setProperty('--ff', '#363636');
   // button expand
-  root.style.setProperty('--bg__btn-expand', '#424242');
+  root.style.setProperty('--bg__btn-expand', '#343333');
   root.style.setProperty('--color__btn-expand', '#fff');
   // modal add new task
-  root.style.setProperty('--bg__modal', '#242424');
-  root.style.setProperty('--bg__modal-input', '#262626');
-  root.style.setProperty('--bg__modal-input-border', '#333333');
-  root.style.setProperty('--bg__modal-inputTitle-border', '#333333');
+  root.style.setProperty('--bg-modal', '#242424');
+  root.style.setProperty('--bg-modal-input', '#262626');
+  root.style.setProperty('--bg-modal-input-border', '#333333');
+  root.style.setProperty('--bg-modal-inputTitle-border', '#333333');
   // task
   root.style.setProperty('--border__task', 'transparent');
+  root.style.setProperty('--bg-task', '#202020');
   // shadows
   root.style.setProperty('--shadow-task', 'transparent');
   root.style.setProperty('--shadow-finder', 'transparent');
@@ -43,12 +44,13 @@ export function makeLight() {
   root.style.setProperty('--bg__btn-expand', '#f2f2f7');
   root.style.setProperty('--color__btn-expand', '#181818');
   // modal add new task
-  root.style.setProperty('--bg__modal', '#f2f2f7');
-  root.style.setProperty('--bg__modal-input', '#fff');
-  root.style.setProperty('--bg__modal-input-border', '#c9c9c9');
-  root.style.setProperty('--bg__modal-inputTitle-border', '#c9c9c9');
+  root.style.setProperty('--bg-modal', '#f2f2f7');
+  root.style.setProperty('--bg-modal-input', '#fff');
+  root.style.setProperty('--bg-modal-input-border', '#c9c9c9');
+  root.style.setProperty('--bg-modal-inputTitle-border', '#c9c9c9');
   // task
   root.style.setProperty('--border__task', '#c9c9c9');
+  root.style.setProperty('--bg-task', '#fff');
   // shadows
   root.style.setProperty('--shadow-finder', '0px 20px 18px -18px rgba(19, 144, 67, 0.4)');
   root.style.setProperty('--shadow-task', '0px 22px 16px -21px rgb(42 153 47 / 44%)');
