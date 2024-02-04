7906c5a228dbf64cde088fc2eb9269fa4612f3d6
ValenciaArcega <valencia.arcega@gmail.com>
2024-02-03 21:38:52 -0600

    css modules on landing page

--- src/views/initial/LandingPage.jsx
+++ src/views/initial/LandingPage.jsx
@@ -4,40 +4,41 @@
  is the main page to introduce "Fare"
  * @author Valencia Arcega Luis Angel
  */
+import css from "./LandingPage.module.css"
 import { IconPanLock, IconBulb, IconMagnify } from '../../components/icons/landing-page'
 
 export function LandingPage() {
-  return <section className="container-landing">
-    <header className="landing-header">
-      <img src="faRe.svg" alt="Fare logo on landing page" />
-      <h1 className="landing-header-title">¡Que las ideas no escapen!</h1>
-    </header>
+	return <section>
+		<header className={css.landingHeader}>
+			<img src="faRe.svg" alt="Fare logo on landing page" />
+			<h1 className={css.landingHeaderTitle}>¡Que las ideas no escapen!</h1>
+		</header>
 
-    <main className="landing-features">
-      <h2 className="landing-features-h2">Guarda un Fare</h2>
-      <p className="landing-features-description">"Idea" ó "Quehacer" (Fare en italiano). Podrás guardar esas pequeñas o grandes tareas en un sólo lugar. Un espacio perfecto para que las ocurrencias se mantengan en el tiempo y las visites cuando quieras.</p>
-      <div className="landing-wrapper-features">
+		<main>
+			<h2 className={css.landingFeaturesTitle}>Guarda un Fare</h2>
+			<p className={css.landingFeaturesDescription}>"Idea" ó "Quehacer" (Fare en italiano). Podrás guardar esas pequeñas o grandes tareas en un sólo lugar. Un espacio perfecto para que las ocurrencias se mantengan en el tiempo y las visites cuando quieras.</p>
 
-        <article className="landing-feature">
-          <IconPanLock />
-          <h3>Sesiones</h3>
-          <p>Solo tu, eres el autor intelectual de tu mente</p>
-        </article>
+			<div className={css.landingWrapperFeatures}>
+				<article className={css.landingFeature}>
+					<IconPanLock />
+					<h3>Sesiones</h3>
+					<p>Solo tu, eres el autor intelectual de tu mente</p>
+				</article>
 
-        <article className="landing-feature">
-          <IconBulb />
-          <h3>Instantánea</h3>
-          <p>Captura y revisa tus ideas al momento de pensarlas</p>
-        </article>
+				<article className={css.landingFeature}>
+					<IconBulb />
+					<h3>Instantánea</h3>
+					<p>Captura y revisa tus ideas al momento de pensarlas</p>
+				</article>
 
-        <article className="landing-feature">
-          <IconMagnify />
-          <h3>No pierdas nada</h3>
-          <p>Busca entre tu mente con indexado de alta velocidad</p>
-        </article>
-      </div>
+				<article className={css.landingFeature}>
+					<IconMagnify />
+					<h3>No pierdas nada</h3>
+					<p>Busca entre tu mente con indexado de alta velocidad</p>
+				</article>
+			</div>
 
-      <article className="fillGradient"></article>
-    </main>
-  </section>
+			<article className={css.fillGradient}></article>
+		</main>
+	</section>
 }
