cc57e20a52de96cf7e5fc067d500f29685c74e12
ValenciaArcega <valencia.arcega@gmail.com>
2024-02-05 17:22:52 -0600

    refactoring: new css module on Login screen

--- src/views/initial/Login.jsx
+++ src/views/initial/Login.jsx
@@ -16,108 +16,106 @@ import { useNavigate } from "react-router-dom"
  * @param {object} setIsRegistering Change between forms in Sign.jsx
  */
 export function Login() {
-  const [isLoginWrong, setIsLoginWrong] = useState(false)
-  const [bannerError, setBannerError] = useState('Las credenciales no coinciden')
-  const cl = new ClSignIn()
-  const navigation = useNavigate()
-  /**
-   * @param {object} e Get the event itself and stored to can
-   * prevent the default behavior in a submit action form
-   */
-  const submitLogIn = async function (e) {
-    e.preventDefault()
-    try {
-      const m = e.target.inputMail.value
-      const p = e.target.inputPassword.value
-      await signInWithEmailAndPassword(auth, m, p)
-    } catch (err) {
-      /**
-      * @throws { err } Handle possible errors that firebase
-      can throw and set custom message for the user
-      */
-      if (err.message === 'Firebase: Error (auth/internal-error).') {
-        setBannerError('Las credenciales no coinciden')
-      }
-      if (err.message === 'Firebase: Error (auth/user-not-found).') {
-        setBannerError('El usuario no existe')
-      }
-      if (err.message === 'Firebase: Error (auth/wrong-password).') {
-        setBannerError('La contraseña es incorrecta')
-      }
-      if (err.message === 'Firebase: Error (auth/invalid-email).') {
-        setBannerError('Campo correo es invalido')
-      }
-      if (err.message === 'Firebase: Error (auth/missing-password).') {
-        setBannerError('Campo contraseña es invalido')
-      }
-      if (err.message === 'Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).') {
-        setBannerError('Sesion bloqueada')
-      }
-      setIsLoginWrong(true)
-      setTimeout(() => setIsLoginWrong(false), 4000)
-    }
-  }
+	const [isLoginWrong, setIsLoginWrong] = useState(false)
+	const [bannerError, setBannerError] = useState('Las credenciales no coinciden')
+	const cl = new ClSignIn()
+	const navigation = useNavigate()
+	/**
+	 * @param {object} e Get the event itself and stored to can
+	 * prevent the default behavior in a submit action form
+	 */
+	const submitLogIn = async function (e) {
+		e.preventDefault()
+		try {
+			const m = e.target.inputMail.value
+			const p = e.target.inputPassword.value
+			await signInWithEmailAndPassword(auth, m, p)
+		} catch (err) {
+			/**
+			* @throws { err } Handle possible errors that firebase
+			can throw and set custom message for the user
+			*/
+			if (err.message === 'Firebase: Error (auth/internal-error).') {
+				setBannerError('Las credenciales no coinciden')
+			}
+			if (err.message === 'Firebase: Error (auth/user-not-found).') {
+				setBannerError('El usuario no existe')
+			}
+			if (err.message === 'Firebase: Error (auth/wrong-password).') {
+				setBannerError('La contraseña es incorrecta')
+			}
+			if (err.message === 'Firebase: Error (auth/invalid-email).') {
+				setBannerError('Campo correo es invalido')
+			}
+			if (err.message === 'Firebase: Error (auth/missing-password).') {
+				setBannerError('Campo contraseña es invalido')
+			}
+			if (err.message === 'Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).') {
+				setBannerError('Sesion bloqueada')
+			}
+			setIsLoginWrong(true)
+			setTimeout(() => setIsLoginWrong(false), 4000)
+		}
+	}
 
-  const renderComponentSignUp = function (e) {
-    e.preventDefault()
-    navigation("/fare/SignUp")
-  }
+	const navigateSignUp = function (e) {
+		e.preventDefault()
+		navigation("/fare/SignUp")
+	}
 
-  return <>
-    <div className={css.containerCircles}>
-      <div className={css.circleLeft}></div>
-      <div className={css.circleRight}></div>
-    </div>
+	return <>
+		<div className={css.containerCircles}>
+			<div className={css.circleLeft}></div>
+			<div className={css.circleRight}></div>
+		</div>
 
-    <section className={css.containerLogin}>
-      {isLoginWrong ? <BadCredentials message={bannerError} /> : null}
+		<section className={css.containerLogin}>
+			{isLoginWrong ? <BadCredentials message={bannerError} /> : null}
 
+			<form className={css.loginForm} onSubmit={submitLogIn}>
+				<img className={css.loginImg} src="login.svg" alt="Icon with a person opening a door to get in" />
+				<h1 className={css.loginTitle}>
+					<span className="gradientText">Inicia Sesión</span></h1>
 
-      <form className="login" onSubmit={submitLogIn}>
-        <img className="login-img" src="login.svg" alt="Icon login" />
-        <h1 className="login-title"><span className="gradientText">Inicia Sesión</span></h1>
+				<label className={css.loginLabel}>Correo electrónico</label>
+				<div className={css.loginWrapperInputMail}>
+					<IconAt />
+					<input
+						id="inputMail"
+						className={css.loginInputMail}
+						type="text"
+						placeholder="usuario@dominio.some"
+						onFocus={() => cl._inputFocusIn('mail', 'mail')}
+						onBlur={() => cl._inputBlur('mail', 'mail')}
+					/>
+				</div>
 
-        <label className="login-label">Correo electrónico</label>
-        <div className="signIn-wrapperInput-mail">
-          <IconAt />
-          <input
-            id="inputMail"
-            className="login-inputMail"
-            type="text"
-            placeholder="usuario@dominio.some"
-            onFocus={() => cl._inputFocusIn('mail', 'mail')}
-            onBlur={() => cl._inputBlur('mail', 'mail')}
-          />
-        </div>
+				<label className={css.loginLabel}>Contraseña</label>
+				<div className={css.loginWrapperInputPassword}>
+					<IconKey />
+					<input
+						id="inputPassword"
+						className={css.loginInputPassword}
+						type="password"
+						autoComplete="new-password"
+						placeholder="Ingresa tu contraseña"
+						onFocus={() => cl._inputFocusIn('pass', 'pass')}
+						onBlur={() => cl._inputBlur('pass', 'pass')}
+					/>
+				</div>
 
-        <label className="login-label">Contraseña</label>
-        <div className="signIn-wrapperInput-pass">
-          <IconKey />
-          <input
-            id="inputPassword"
-            className="login-inputPass"
-            type="password"
-            autoComplete="new-password"
-            placeholder="Ingresa tu contraseña"
-            onFocus={() => cl._inputFocusIn('pass', 'pass')}
-            onBlur={() => cl._inputBlur('pass', 'pass')}
-          />
-        </div>
+				<button className={css.loginBtnAuth} type="submit">
+					<IconCheck />
+					Autenticarme
+				</button>
 
-        <button className="login-buttonLogin" type="submit">
-          <IconCheck />
-          Autenticarme
-        </button>
-
-        <label className="login-labelBGR" htmlFor="lbgr">¿No tienes una cuenta? <button
-          id="lbgr"
-          className="login-BGR"
-          type="button"
-          onClick={renderComponentSignUp}
-        >Registrate
-        </button>
-        </label>
-      </form>
-    </section>
-  </>
+				<label className={css.loginLabelGoSignUp} htmlFor="lbgr">¿No tienes una cuenta? <button
+					id="lbgr"
+					className={css.loginBtnGoSignUp}
+					type="button"
+					onClick={navigateSignUp}>Registrate</button>
+				</label>
+			</form>
+		</section >
+	</>
 }
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite"
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/fare/',
   server: {
     host: '0.0.0.0',
-    open: true
+    open: false
   }
-});
+})
