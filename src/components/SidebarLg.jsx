ea6a15120dc2048cbe19e55aa43edb2b71c4c22f
ValenciaArcega <valencia.arcega@gmail.com>
2024-02-17 22:52:06 -0600

    feature: new sidebar and icons

--- src/components/SidebarLg.jsx
+++ src/components/SidebarLg.jsx
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom"
 import css from "./SidebarLg.module.css"
+import { IconStar, IconProfile, IconMagnify, IconHome } from "./icons/sidebar"
 
 export function SidebarLg() {
-    return <nav className={css.navigationBar}>
+    const navigation = useNavigate()
 
+    function navigateTo(route) {
+        navigation(route)
+    }
+
+    return <nav className={css.navigationBar}>
+        <img src="logo.png" alt="brand simple logo" height={48} />
+
+        <div onClick={() => navigateTo("/fare/")} className={css.wrapperBtn}>
+            <IconHome fill="#181818" height={32} />
+        </div>
     </nav>
 }
