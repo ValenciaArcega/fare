ce88ad566659a5f5a6174fd98381b8fb137cfed3
ValenciaArcega <valencia.arcega@gmail.com>
2024-03-12 18:23:46 -0600

    fixes: some design issues

--- src/views/initial/SignUp.jsx
+++ src/views/initial/SignUp.jsx
@@ -108,7 +108,7 @@ export function SignUp() {
 					onFocus={() => classReview._inputFocusIn('mail')}
 					onBlur={() => classReview._inputBlur('mail')}
 					onChangeCapture={() => classReview._inputMailOnChangeCapture()}
-				/>
+				/>x
 				<p id="signUp-mail-p" className={css.signUpFeedback}></p>
 
 				<label className={css.signUpLabel} htmlFor="suPassword">Contraseña</label>
