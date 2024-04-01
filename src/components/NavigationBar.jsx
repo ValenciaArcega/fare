5a775aef27896594a7612ed87439c320c9deae8d
ValenciaArcega <valencia.arcega@gmail.com>
2024-04-01 13:16:27 -0600

    feature: new navigation bar has been added

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,7 @@
+import s from "../css/NavigationBar.module.css"
+
+export function NavigationBar({ setIsAdding }) {
+	return <nav className={s.navMobile}>
+
+	</nav>
+}
--- src/routes/RouterHome.jsx
+++ src/routes/RouterHome.jsx
@@ -4,13 +4,30 @@ import { Dashboard } from "../views/Dashboard"
 import { Profile } from "../views/Profile"
 import { Favorites } from "../views/Favorites"
 import { SidebarLg } from "../components/SidebarLg"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { NavigationBar } from "../components/NavigationBar"
 
 export function RouterHome() {
+    let viewPort = window.matchMedia("(max-width: 664px)")
+    const [isMobile, setIsMobile] = useState(false)
     const [isAdding, setIsAdding] = useState(false)
 
+    useEffect(function () {
+        const mediaScreen = y => {
+            if (y.matches) {
+                setIsMobile(true)
+            } else {
+                setIsMobile(false)
+            }
+        }
+        mediaScreen(viewPort)
+        viewPort.addListener(mediaScreen)
+    }, [])
+
     return <section className="layoutDashboard">
-        <SidebarLg setIsAdding={setIsAdding} />
+        {isMobile
+            ? <NavigationBar setIsAdding={setIsAdding} />
+            : <SidebarLg setIsAdding={setIsAdding} />}
 
         <Routes>
             <Route path="/fare/"
